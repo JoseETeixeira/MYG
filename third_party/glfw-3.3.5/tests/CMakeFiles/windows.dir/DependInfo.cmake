8f0d63fc5440547888f175c8901ebe31fa5bdbd2
Jose_Eduardo <joseeduardot@gmail.com>
2021-11-12 15:16:33 -0300

    glfw

---
+++ third_party/glfw-3.3.5/tests/CMakeFiles/windows.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/eduardo/Desktop/BYOND/MYG.gg/MYG/glfw-3.3.5/deps/getopt.c" "/home/eduardo/Desktop/BYOND/MYG.gg/MYG/glfw-3.3.5/tests/CMakeFiles/windows.dir/__/deps/getopt.c.o"
+  "/home/eduardo/Desktop/BYOND/MYG.gg/MYG/glfw-3.3.5/deps/glad_gl.c" "/home/eduardo/Desktop/BYOND/MYG.gg/MYG/glfw-3.3.5/tests/CMakeFiles/windows.dir/__/deps/glad_gl.c.o"
+  "/home/eduardo/Desktop/BYOND/MYG.gg/MYG/glfw-3.3.5/tests/windows.c" "/home/eduardo/Desktop/BYOND/MYG.gg/MYG/glfw-3.3.5/tests/CMakeFiles/windows.dir/windows.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "deps"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/eduardo/Desktop/BYOND/MYG.gg/MYG/glfw-3.3.5/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
