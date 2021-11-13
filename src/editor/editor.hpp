06c6458da962882b287eb97786d6e5dd3f352af2
José Eduardo <joseeduardot@gmail.com>
2021-11-13 12:43:00 -0300

    adds imgui

--- src/editor/editor.hpp
+++ src/editor/editor.hpp
@@ -5,11 +5,16 @@
 
 
 #pragma once
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "../../third_party/glfw/deps/glad/gl.h"
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
  
-
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
  
 
 
@@ -33,7 +38,15 @@ namespace myg{
 
             int xpos,ypos,width,height;
 
+            const char* glsl_version = "#version 130";
+
+            bool show_demo_window = true;
+            bool show_another_window = false;
+            ImVec4 clear_color = ImVec4(0.07f,0.13f,0.17f,1.0f);
+
             void InitializeGLFW(bool fullscreen);
+
+            void InitializeImGUI();
 
             void MainLoop();
 
