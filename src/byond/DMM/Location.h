eef327bbfceac690247852ef1ff5e95fc74be4fd
Jose_Eduardo <joseeduardot@gmail.com>
2021-12-01 11:01:01 -0300

    location

--- src/byond/DMM/Location.h
+++ src/byond/DMM/Location.h
@@ -1,71 +1,69 @@
-package com.github.tgstation.fastdmm.dmmmap;
-
-/**
- * Represents a 3-dimensional position in a DMM file.
- */
-public class Location {
-
-    public int x;
-    public int y;
-    public int z;
+namespace BYOND::dmm{
 
     /**
-     * Constructs a new Location with the given coordinates
-     *
-     * @param x The x-coordinate of this new location
-     * @param y The y-coordinate of this new location
-     * @param z The z-coordinate of this new location
+     * Represents a 3-dimensional position in a DMM file.
      */
-    public Location(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+    class Location {
 
-    /**
-     * Updates the Location with the given coordinates
-     *
-     * @param x The x-coordinates
-     * @param y The y-coordinates
-     * @param z The z-coordinates
-     */
-    public void set(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+    public:
+        int x;
+        int y;
+        int z;
 
-    /**
-     * Takes a step into the given direction.
-     *
-     * @param dir The direction to step into.
-     * @return The new location.
-     */
-    public Location getStep(int dir) {
-        Location l = new Location(x, y, z);
-        if ((dir & 1) != 0)
-            l.y++;
-        if ((dir & 2) != 0)
-            l.y--;
-        if ((dir & 4) != 0)
-            l.x++;
-        if ((dir & 8) != 0)
-            l.x--;
-        return l;
-    }
-    
-    @Override
-    public boolean equals(Object other) {
-    	if(!(other instanceof Location))
-    		return false;
-    	if(other == this)
-    		return false;
-    	Location o = (Location)other;
-    	return x == o.x && y == o.y && z == o.z;
-    }
-    
-    @Override
-    public int hashCode() {
-    	return (x<<16) + (y<<8) + (z);
-    }
-}
+        /**
+         * Constructs a new Location with the given coordinates
+         *
+         * @param x The x-coordinate of this new location
+         * @param y The y-coordinate of this new location
+         * @param z The z-coordinate of this new location
+         */
+        Location(int x, int y, int z) {
+            this->x = x;
+            this->y = y;
+            this->z = z;
+        }
+
+        /**
+         * Updates the Location with the given coordinates
+         *
+         * @param x The x-coordinates
+         * @param y The y-coordinates
+         * @param z The z-coordinates
+         */
+        void set(int x, int y, int z) {
+            this->x = x;
+            this->y = y;
+            this->z = z;
+        }
+
+        /**
+         * Takes a step into the given direction.
+         *
+         * @param dir The direction to step into.
+         * @return The new location.
+         */
+        Location* getStep(int dir) {
+            Location *l = new Location(x, y, z);
+            if ((dir & 1) != 0)
+                l->y++;
+            if ((dir & 2) != 0)
+                l->y--;
+            if ((dir & 4) != 0)
+                l->x++;
+            if ((dir & 8) != 0)
+                l->x--;
+            return l;
+        }
+        
+
+        bool equals(Location* other) {
+            if(other == this)
+                return true;//TODO: era false
+            return x == other->x && y == other->y && z == other->z;
+        }
+        
+        int hashCode() {
+            return (x<<16) + (y<<8) + (z);
+        }
+    };
+};
